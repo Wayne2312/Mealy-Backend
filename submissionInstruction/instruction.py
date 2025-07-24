c4380fe6a04c7759d49bb0c4b5745f438d5bdd8d
Wayne2312 <Wayne.Kimani.805@gmail.com>
2025-07-24 23:21:02 +0300

    Follow instructions and submit your work
    Instructions in Instructions.py

---
+++ submissionInstruction/instruction.py
@@ -0,0 +1,9 @@
+#here are instructions
+#set up and configure the database on your computer
+#use the app folder to test your work and components
+#add your work in the app folder 
+#if other files are added, add them in this folder
+#add meaningful comments and disclaimers in your code
+#highlight issues faced in summarized form
+#do not start new subfolders in the app folder or these folder
+#ommit virtual environments in your
